d8289b025686d2fbe6445b7d2eb02225e88f2b62
jr-garcia <jr-garcia@users.noreply.github.com>
2018-03-07 02:39:30 -0600

    Add Assimp download and build..

--- ci_scripts/assimpDownload.py
+++ ci_scripts/assimpDownload.py
@@ -4,26 +4,6 @@ import zipfile
 from os import environ as env
 from subprocess import check_output
 
-if not path.exists('downloads'):
-    mkdir('downloads')
-
-PYTHON = env.get("PYTHON", sys.executable)
-
-dest = path.join('downloads', 'assimp.zip')
-
-if not path.exists(dest):
-    check_output("{} -m p ip install requests -f downloads --cache-dir downloads".format(PYTHON).split())
-    from requests import get
-
-    with open(dest, "wb") as file:
-        response = get('https://github.com/assimp/assimp/archive/v4.1.0.zip')
-        file.write(response.content)
-
-if not path.exists('assimp_unzipped'):
-    zip_ref = zipfile.ZipFile(dest, 'r')
-    zip_ref.extractall('assimp_unzipped')
-    zip_ref.close()
-
 platform = sys.platform
 includes = []
 
@@ -37,9 +17,37 @@ elif platform == 'darwin':
 else:
     includes.extend(['/usr/include/assimp', '/usr/local/include/assimp'])
 
-for iPath in includePath:
+for iPath in includes:
     if path.exists(iPath):
         exit(0)
+
+print('compiled assimp not found. Building...')
+
+import installPandoc
+
+if not path.exists('downloads'):
+    mkdir('downloads')
+
+PYTHON = env.get("PYTHON", sys.executable)
+
+dest = path.join('downloads', 'assimp.zip')
+
+if not path.exists(dest):
+    print('assimp zip not found. Downloading...')
+    check_output("{} -m pip install requests -f downloads --cache-dir downloads".format(PYTHON).split())
+    from requests import get
+
+    with open(dest, "wb") as file:
+        response = get('https://github.com/assimp/assimp/archive/v4.1.0.zip')
+        file.write(response.content)
+
+if not path.exists('assimp_unzipped'):
+    print('unpacked assimp not found. Unpacking...')
+    zip_ref = zipfile.ZipFile(dest, 'r')
+    zip_ref.extractall('assimp_unzipped')
+    zip_ref.close()
+
+
 
 if platform == 'win32':
     pass
