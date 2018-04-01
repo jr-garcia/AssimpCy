8db4213d20486a60abda1ba486438f54c3b830c0
jr-garcia <jr-garcia@users.noreply.github.com>
2018-03-31 20:40:55 -0600

    Fix build wheels with Pandoc.

--- ci_scripts/installPandoc.py
+++ ci_scripts/installPandoc.py
@@ -3,24 +3,37 @@ from subprocess import call, check_output
 import sys
 from shutil import copy2
 
+platform = sys.platform
+
 
 def checkAndInstall():
     try:
         check_output('pandoc -v'.split())
     except OSError:
         cudir = os.path.abspath(os.curdir)
-        os.chdir('downloads')
+        os.chdir(os.path.abspath(os.path.join(os.path.pardir, 'downloads')))
 
-        from requests import get
+        def getFile():
+            from requests import get
+            with open(pandocFile, "wb") as file:
+                response = get(source)
+                file.write(response.content)
 
-        pandocFile = 'pandoc-2.1.3-linux.tar.gz'
-
-        with open(pandocFile, "wb") as file:
-            response = get('https://github.com/jgm/pandoc/releases/download/2.1.3/pandoc-2.1.3-linux.tar.gz')
-            file.write(response.content)
-
-        call("tar -xvzf {}".format(pandocFile).split())
-        copy2('./pandoc-2.1.3/bin/pandoc', '/usr/local/bin')
-        copy2('./pandoc-2.1.3/bin/pandoc-citeproc', '/usr/local/bin')
+        if platform == 'win32':
+            pandocFile = 'pandoc-2.1.3-windows.msi'
+            source = 'https://github.com/jgm/pandoc/releases/download/2.1.3/' + pandocFile
+            getFile()
+            call('msiexec.exe /i "{}" /norestart'.format(pandocFile))
+        else:
+            pandocFile = 'pandoc-2.1.3-linux.tar.gz'
+            source = 'https://github.com/jgm/pandoc/releases/download/2.1.3/' + pandocFile
+            getFile()
+            call("tar -xvzf {}".format(pandocFile).split())
+            copy2('./pandoc-2.1.3/bin/pandoc', '/usr/local/bin')
+            copy2('./pandoc-2.1.3/bin/pandoc-citeproc', '/usr/local/bin')
 
         os.chdir(cudir)
+
+
+if __name__ == '__main__':
+    checkAndInstall()
